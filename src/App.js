ac77f6069adc1fc1e1b0b9a44fcd2c659d0cc7fd
Vse Putyom <bertraid777@gmail.com>
2021-10-27 19:55:46 +0300

    firebase init Sweet Art

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCclR1cOUO4JtIQ-2j2xmc7RbQ0YZUnsTU",
+  authDomain: "sweetart-70163.firebaseapp.com",
+  projectId: "sweetart-70163",
+  storageBucket: "sweetart-70163.appspot.com",
+  messagingSenderId: "22431365291",
+  appId: "1:22431365291:web:c7bd87af34d391ed2c9fee",
+  measurementId: "G-34LXRCKV94"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
   return (
